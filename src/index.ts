79709cb3801cec97e90cd8394aa7b1450c5793a9
Matheus Felipe <theusf40@hotmail.com>
2026-05-03 08:24:10 -0300

    fix: modificado local de escrita para result.json

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ function main() {
     const data = readData('data.json');
     const result = processData(data);
 
-    writeData('output.json', result);
+    writeData('result.json', result);
     console.log('Processamento concluido!');
 }
 
